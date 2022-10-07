af577fe392b1e370332f5133df868c0687cd0ae2
Daniel Aparicio <dapariciom@outlook.com>
2022-10-07 13:55:13 -0500

    Added login test

--- src/test/java/com/example/shorturl/controller/login/AuthControllerTest.java
+++ src/test/java/com/example/shorturl/controller/login/AuthControllerTest.java
@@ -1,12 +1,15 @@
 package com.example.shorturl.controller.login;
 
+import com.example.shorturl.model.auth.AuthenticationRequest;
+import com.example.shorturl.model.auth.AuthenticationResponse;
 import com.example.shorturl.model.roles.ERole;
 import com.example.shorturl.model.roles.RoleEntity;
 import com.example.shorturl.model.user.UserEntity;
 import com.example.shorturl.model.user.UserRequest;
 import com.example.shorturl.model.user.UserResponse;
-import com.example.shorturl.security.jwt.JwtUtil;
+import com.example.shorturl.security.MyUserDetails;
 import com.example.shorturl.security.MyUserDetailsService;
+import com.example.shorturl.security.jwt.JwtUtil;
 import com.example.shorturl.service.user.UserService;
 import com.google.common.collect.ImmutableSet;
 import org.junit.jupiter.api.Test;
@@ -19,9 +22,15 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -42,6 +51,10 @@ public class AuthControllerTest {
     private MyUserDetailsService myUserDetailsService;
     @Mock
     private JwtUtil jwtUtil;
+    @Mock
+    private Authentication authentication;
+    @Mock
+    private SecurityContext securityContext;
 
     @Test
     void testSignUpWithCorrectUser(){
@@ -134,6 +147,58 @@ public class AuthControllerTest {
 
         assertEquals(errorMessage, exception.getMessage());
 
+    }
+
+    @Test
+    void testLoginWithCorrectCredentials() {
+
+        UserEntity user = new UserEntity(1L, "user", "user", "user@outlook.com", "user", "user", ImmutableSet.of(RoleEntity.builder()
+                .id(2L)
+                .name(ERole.ROLE_USER)
+                .build()));
+
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest(user.getUserName(), user.getPassword());
+
+        MyUserDetails myUserDetails = new MyUserDetails(1L, user.getUserName(),user.getEmail(), user.getPassword(), user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList()));
+
+        String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjY1MjAwMDQ0LCJpYXQiOjE2NjUxNjQwNDR9.V1XkSvuVZuxrJS5ZGAOdmPbC7YGhQlg2jhYQR6DwKM4";
+
+        when(authenticationManager.authenticate(any())).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+
+        when(authentication.getPrincipal()).thenReturn(myUserDetails);
+
+        when(jwtUtil.generateToken(any())).thenReturn(jwt);
+
+        ResponseEntity<AuthenticationResponse> responseObtained = authController.createAuthenticationToken(authenticationRequest);
+
+        assertEquals(responseObtained.getBody().getUserName(), user.getUserName());
+        assertEquals(responseObtained.getBody().getEmail(), user.getEmail());
+        assertEquals(responseObtained.getBody().getJwt(), jwt);
+        assertEquals(responseObtained.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    void testLoginWithIncorrectCredentials() {
+
+        UserEntity user = new UserEntity(1L, "user", "user", "user@outlook.com", "user", "user", ImmutableSet.of(RoleEntity.builder()
+                .id(2L)
+                .name(ERole.ROLE_USER)
+                .build()));
+
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest(user.getUserName(), "password");
+
+        String errorMessage = "Bad credentials";
+
+        when(authenticationManager.authenticate(any())).thenThrow(new BadCredentialsException(errorMessage));
+
+        Executable executable = () -> authController.createAuthenticationToken(authenticationRequest);
+
+        BadCredentialsException exception = assertThrows(BadCredentialsException.class, executable);
+
+        assertEquals(errorMessage, exception.getMessage());
     }
 
 }
